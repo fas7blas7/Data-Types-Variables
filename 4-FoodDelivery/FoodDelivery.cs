4fc980be28a193bc13da455ef3bc3d079935d406
Martin Kirov <fas7blas7@gmail.com>
2025-03-06 21:11:41 +0200

    Adding FoodDelivery task

---
+++ 4-FoodDelivery/FoodDelivery.cs
@@ -0,0 +1,25 @@
+﻿namespace _4_FoodDelivery
+{
+    internal class FoodDelivery
+    {
+        static void Main(string[] args)
+        {
+            int chickenMenu = int.Parse(Console.ReadLine());
+            int fishMenu = int.Parse(Console.ReadLine());
+            int vegetarianMenu = int.Parse(Console.ReadLine());
+
+            double chickenMenuPrice = 10.35;
+            double fishMenuPrice = 12.40;
+            double vegetarianMenuPrice = 8.15;
+            double deliveryPrice = 2.50;
+
+            double total = chickenMenu * chickenMenuPrice + fishMenu * fishMenuPrice + vegetarianMenu * vegetarianMenuPrice;
+
+            double desertPrice = total * 0.20;
+
+            double grandTotal = total + desertPrice + deliveryPrice;
+
+            Console.WriteLine(grandTotal);
+        }
+    }
+}
