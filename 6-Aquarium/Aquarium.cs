b56e23ee3512b2eaeb95ac4784463444a068f918
Martin Kirov <fas7blas7@gmail.com>
2025-03-13 21:28:57 +0200

    Adding Aquarium task

---
+++ 6-Aquarium/Aquarium.cs
@@ -0,0 +1,19 @@
+﻿namespace _6_Aquarium
+{
+    internal class Aquarium
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+
+            double volume = length * width * height;
+            double totalLiters = volume * 0.001;
+            double percentageDecimal = percentage * 0.01;
+            double totalLitersNeeded = totalLiters * (1 - percentageDecimal);
+            Console.WriteLine($"{totalLitersNeeded:F2}");
+        }
+    }
+}
