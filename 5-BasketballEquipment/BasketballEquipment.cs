f63f57d0e44c39a8eadf60a31b87efb12839d7e4
Martin Kirov <fas7blas7@gmail.com>
2025-03-13 21:27:58 +0200

    Adding BasketballEquipment task

---
+++ 5-BasketballEquipment/BasketballEquipment.cs
@@ -0,0 +1,19 @@
+﻿namespace _5_BasketballEquipment
+{
+    internal class BasketballEquipment
+    {
+        static void Main(string[] args)
+        {
+            int yearFee = int.Parse(Console.ReadLine());
+
+            double shoesPrice = yearFee * 0.60;
+            double clothesPrice = shoesPrice * 0.80;
+            double ballPrice = clothesPrice / 4;
+            double accessoriesPrice = ballPrice / 5;
+
+            double total = yearFee + shoesPrice + clothesPrice + ballPrice + accessoriesPrice;
+
+            Console.WriteLine(total);
+        }
+    }
+}
